8774766529394c875c22ef5fbe1e1b66d1fb1642
Ryan Kuederle <ryankuederle@icloud.com>
2025-02-02 19:09:33 -0500

    Added venv, updated kinematics.

--- basic_arm_modeling/sim.py
+++ basic_arm_modeling/sim.py
@@ -9,46 +9,66 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 plane = p.loadURDF("plane.urdf")
 robot = p.loadURDF("arm.urdf")
 p.resetDebugVisualizerCamera(cameraDistance=1.7, cameraYaw=50, cameraPitch=-35, cameraTargetPosition=[0, 0, 0])
-
+L = 0.5
 p.setGravity(0,0,-10)
 num_joints = p.getNumJoints(robot)
 joint_sliders = []
 
-def get_positions(dist):
-    """
-    If we have a position, x, we can think of this as an equillateral triangle. 
 
-    Thus, use the law of cosines to find the angles of the triangle.
-    """
-    arm_length = 0.5
-    # Using the law of cosines to find the third side and angles
-    a = arm_length
-    b = arm_length
-    c = 1 - dist
+def base_link1_angle(angle):
+    return math.pi/2 - angle
 
-    # Calculate the angle opposite to side c
-    angle_c = math.acos((a**2 + b**2 - c**2) / (2 * a * b))
+def link1_link2_angle(angle):
+    return math.pi - angle
 
-    # Calculate the angles opposite to sides a and b
-    angle_a = math.acos((b**2 + c**2 - a**2) / (2 * b * c))
-    angle_b = math.acos((a**2 + c**2 - b**2) / (2 * a * c))
+def angles_by_extent(r):
+    # Ensure the value is within the valid range for acos
+    value = (r/2)/L
+    value = max(min(value, 1), -1)
+    angle_1 = math.acos(value)
+    angle_2 = 2 * (math.pi/2 - angle_1)
+    return angle_1, angle_2
 
-    return angle_a, angle_c
+def angle_height_adjust(extent, height):
+    return math.atan2(height, extent)
 
-for i in range(num_joints):
-    joint_info = p.getJointInfo(robot, i)
-    joint_name = joint_info[1].decode('utf-8')
-    joint_sliders.append(p.addUserDebugParameter(joint_name, -3, 3, 0))
-get_positions(0.3)
+def cartesian_to_polar(x, y):
+    r = math.sqrt(x**2 + y**2)
+    theta = math.atan2(y, x)
+    return r, theta
 
-distance_slider = p.addUserDebugParameter("Distance", 0, 1, 0.5)
+def angle_2_fixed(angle_1, angl1_2):
+    return math.pi - angle_1 - angle_2
 
+rotation_slider = p.addUserDebugParameter("rotation", -math.pi, math.pi, 0)
+extent_slider = p.addUserDebugParameter("extent", 0, 2*L, 0)
+height_slider = p.addUserDebugParameter("height", -1*L, 1, 0)
+wrist_slider = p.addUserDebugParameter("wrist", -math.pi, math.pi, 0)
+hand_slider = p.addUserDebugParameter("hand", -math.pi, math.pi, 0)
+
+r_last = 0
 while True:
-    a = get_positions(p.readUserDebugParameter(distance_slider))
-    # target_positions = [p.readUserDebugParameter(slider) for slider in joint_sliders]
-    target_positions = [p.readUserDebugParameter(joint_sliders[0]), a[0], a[1]]
-    p.setJointMotorControlArray(robot, range(num_joints), p.POSITION_CONTROL, targetPositions=target_positions)
-    print(a)
+    try:
+        rotation_angle = p.readUserDebugParameter(rotation_slider)
+        height = p.readUserDebugParameter(height_slider)
+        extent = p.readUserDebugParameter(extent_slider)
+        wrist_angle = p.readUserDebugParameter(wrist_slider)
+        hand_angle = p.readUserDebugParameter(hand_slider)
+        r, theta = cartesian_to_polar(extent, height)
+        angle_1, angle_2 = angles_by_extent(r)
+        angle_1 += theta
+        angle_joint_1 = angle_1
+        angle_joint_2 = angle_2
+        angle_wrist = angle_2_fixed(angle_1, angle_2) + wrist_angle
+        
+        print(f"r: {r:.2f}, theta: {theta:.2f}, angle_joint_1: {angle_joint_1:.2f}, angle_joint_2: {angle_joint_2:.2f}")
+        target_positions = [rotation_angle, base_link1_angle(angle_joint_1), link1_link2_angle(angle_joint_2), -angle_wrist, -hand_angle]
+        p.setJointMotorControlArray(robot, range(num_joints), p.POSITION_CONTROL, targetPositions=target_positions)
+        
+    
+    except:
+        pass
     p.stepSimulation()
-    time.sleep(1./240)
+    time.sleep(1./60)
+
 
